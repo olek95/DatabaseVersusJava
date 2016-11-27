004f14d95d22464695e2784b5928a1efdb8afc4a
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-27 23:32:08 +0100

    Update SQLSorting.java

--- SQLSorting.java
+++ SQLSorting.java
@@ -7,12 +7,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.binding.StringExpression;
+import javafx.beans.property.StringProperty;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class SQLSorting implements Runnable{
-    private DatabaseVersusJavaFXMLController controller;
-    public SQLSorting(DatabaseVersusJavaFXMLController cotroller){
-        this.controller = cotroller;
+    //private DatabaseVersusJavaFXMLController controller;
+    //public SQLSorting(DatabaseVersusJavaFXMLController cotroller){
+    //    this.controller = cotroller;
+    //}
+    private StringProperty textAreaTextProperty, textFieldTextProperty;
+    public SQLSorting(StringProperty textAreaTextProperty, StringProperty textFieldTextProperty){
+        this.textAreaTextProperty = textAreaTextProperty;
+        this.textFieldTextProperty = textFieldTextProperty;
     }
     public void run(){
         try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/databaseversusjava", "olek", "haslo12345")){
@@ -20,11 +28,12 @@ public class SQLSorting implements Runnable{
             long before = System.currentTimeMillis();
             ResultSet rs = stat.executeQuery("SELECT * FROM Dane ORDER BY id, data DESC");
             long after = System.currentTimeMillis();
-            TextArea databaseTextArea = controller.getDatabaseTextArea();
-            databaseTextArea.setText("");
-            while(rs.next())
-                databaseTextArea.appendText(rs.getLong("id") + " " + rs.getLong("data") +"\n");
-            controller.getDatabaseTimeTextField().setText((double)(after - before)/1000 + " s");
+            StringBuffer sb = new StringBuffer("");
+            while(rs.next()){
+                sb.append(rs.getLong("id")).append(" ").append(rs.getLong("data")).append("\n");
+            }
+            textAreaTextProperty.set(sb.toString());
+            textFieldTextProperty.set((double)(after - before)/1000 + " s");
         }catch(SQLException e){
             Logger.getLogger(DatabaseVersusJavaFXMLController.class.getName()).log(Level.SEVERE, null, e);
         }
